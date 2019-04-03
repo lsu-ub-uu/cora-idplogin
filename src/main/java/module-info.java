5893920a07ee2cd5ccc0f24ddc4204fab46a34cd
olovm <olov.mckie@ub.uu.se>
2019-04-03 09:56:33 +0000

    updating for use of java modules

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module se.uu.ub.cora.idplogin {
+	requires se.uu.ub.cora.gatekeepertokenprovider;
+	requires se.uu.ub.cora.json;
+	requires java.activation;
+	requires java.ws.rs;
+	requires javax.servlet.api;
+	requires encoder;
+}
