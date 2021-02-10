a06d708bb5d19d8247a41fe9185f122ecf091c3a
olovm <olov.mckie@ub.uu.se>
2021-02-10 09:11:41 +0000

    fixing test not running from ide

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module se.uu.ub.cora.idplogin {
 	requires se.uu.ub.cora.logger;
 	requires se.uu.ub.cora.gatekeepertokenprovider;
 	requires se.uu.ub.cora.json;
-	requires jakarta.activation;
+	requires java.activation;
 	requires java.ws.rs;
 	requires javax.servlet.api;
 	requires encoder;
