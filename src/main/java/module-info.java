026a9f4c847bafd976afd9546d20b3a504541e3e
olovm <olov.mckie@ub.uu.se>
2022-01-19 14:14:57 +0000

    fixing sonar complaints

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module se.uu.ub.cora.idplogin {
 	requires se.uu.ub.cora.logger;
 	requires se.uu.ub.cora.gatekeepertokenprovider;
 	requires se.uu.ub.cora.json;
-	// requires jakarta.activation;
-	// requires java.ws.rs;
 	requires encoder;
 	requires jakarta.servlet;
 }
